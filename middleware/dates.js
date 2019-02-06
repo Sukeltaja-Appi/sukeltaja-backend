b110688e60ee290039a86527f32e61f556f5708b
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-02-06 14:09:15 +0200

    don't allow enddate to be less than startdate

---
+++ middleware/dates.js
@@ -0,0 +1,12 @@
+const DateTime = require('luxon').DateTime
+
+const handleEndDate = (startdate, enddate) => {
+  // don't allow enddate to be less than or equal to startdate
+  if (!enddate || enddate <= startdate) {
+    return DateTime.fromISO(startdate).plus({ hours: 1 }).toJSDate()
+  }
+
+  return enddate
+}
+
+module.exports = handleEndDate
