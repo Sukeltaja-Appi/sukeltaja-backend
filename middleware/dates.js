451dbcffacf16a44e2de9fd52e95a3a193b4d123
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-02-07 01:10:52 +0200

    convert from iso if given date type is string

--- middleware/dates.js
+++ middleware/dates.js
@@ -3,7 +3,11 @@ const DateTime = require('luxon').DateTime
 const handleEndDate = (startdate, enddate) => {
   // don't allow enddate to be less than or equal to startdate
   if (!enddate || enddate <= startdate) {
-    return DateTime.fromISO(startdate).plus({ hours: 1 }).toJSDate()
+    if (typeof startdate === 'string') {
+      return DateTime.fromISO(startdate).plus({ hours: 1 }).toJSDate()
+    }
+
+    return DateTime.fromJSDate(startdate).plus({ hours: 1 }).toJSDate()
   }
 
   return enddate
