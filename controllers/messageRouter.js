9249d534e46c80f633b16d007eaf99a9c939c92d
wametsol <waltteri.metsola@gmail.com>
2019-02-28 13:50:09 +0200

    added /put method for message

--- controllers/messageRouter.js
+++ controllers/messageRouter.js
@@ -66,4 +66,30 @@ messageRouter.post('/', async (req, res) => {
     }
 })
 
+messageRouter.put('/:id', async (req, res) => {
+    try {
+      const { recieved } = req.body
+  
+      if (!recieved) {
+        return res.status(400).json({ error: 'missing fields' })
+      }
+  
+      const updatedMessage = await Message.findByIdAndUpdate(
+        req.params.id,
+        { recieved },
+        { new: true }
+      )
+  
+      res.json(Message.format(updatedMessage))
+  
+    } catch (exception) {
+      if (exception.name === 'JsonWebTokenError') {
+        res.status(401).json({ error: exception.message })
+      } else {
+        console.log(exception)
+        res.status(500).json({ error: 'something went wrong...' })
+      }
+    }
+  })
+
 module.exports = messageRouter
