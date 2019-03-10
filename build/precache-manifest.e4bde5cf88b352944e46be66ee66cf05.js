4bdaf5231cafb45c17e9e9e03d025e94943d66ec
majormalfunk <32271489+majormalfunk@users.noreply.github.com>
2019-03-10 21:38:35 +0200

    Back office build added

---
+++ build/precache-manifest.e4bde5cf88b352944e46be66ee66cf05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3e4807eec7cec866b6fb",
+    "url": "/static/js/main.3e4807ee.chunk.js"
+  },
+  {
+    "revision": "4f298afbad3eb23a4720",
+    "url": "/static/js/2.4f298afb.chunk.js"
+  },
+  {
+    "revision": "3e4807eec7cec866b6fb",
+    "url": "/static/css/main.4b816e88.chunk.css"
+  },
+  {
+    "revision": "24313294bb27d614502fc369f46374a3",
+    "url": "/index.html"
+  }
+];
