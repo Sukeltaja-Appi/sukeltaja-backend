06a7ea730f7d8920c4784932bb03d75eb3c154e3
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2019-03-13 15:48:30 +0200

    refactor and add new tests to event & user
    There are still some failing tests: 1) shouldn't be able to delete events you don't own 2) proper status codes

---
+++ tests/helpers/testHelper.js
@@ -0,0 +1,57 @@
+const User = require('../../models/user')
+const Event = require('../../models/event')
+const Dive = require('../../models/dive')
+const Target = require('../../models/target')
+const { initialUsers, initialEvents, initialTargets, initialDives, nonExistingEventId } = require('./initialData')
+const { login, post } = require('./helperFunctions')
+
+const [ firstEvent, secondEvent, thirdEvent ] = initialEvents
+
+// Helper functions
+const eventsInDb = async () => {
+  const events = await Event.find({})
+
+  return events.map(Event.format)
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+
+  return users.map(User.format)
+}
+const clearDb = async () => {
+  await User.deleteMany({})
+  await Event.deleteMany({})
+  await Target.deleteMany({})
+  await Dive.deleteMany({})
+}
+
+const postUsers = async () => post.data(Object.values(initialUsers)).toUrl('users').exec()
+
+const postEvents = async () => post.data([firstEvent, secondEvent]).toUrl('events').asUser('SamiSukeltaja').exec()
+
+const postThirdEventAsKalle = async () => post.data(thirdEvent).toUrl('events').asUser('KalleKalastaja').exec()
+
+const postDives = async () => post.data(initialDives).toUrl('dives').asUser('SamiSukeltaja').exec()
+
+const postTargets = async () => post.data(initialTargets).toUrl('targets').asUser('SamiSukeltaja').exec()
+
+const mapEventIdsToDives = (events) => initialDives.map((dive, index) => dive.event = `${events[index]._id}`)
+
+// Main function
+const initializeDb = async () => {
+  await clearDb()
+  await postUsers()
+
+  const restOfEvents = await postEvents()
+  const lastEvent = await postThirdEventAsKalle()
+
+  mapEventIdsToDives([...restOfEvents, lastEvent])
+
+  await postDives()
+  await postTargets()
+}
+
+module.exports = {
+  eventsInDb, initialEvents, initialUsers, initializeDb, nonExistingEventId, usersInDb, login
+}
