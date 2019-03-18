44c207c862b39df085eae09ea84318d5d644286d
wametsol <waltteri.metsola@gmail.com>
2019-03-18 20:48:59 +0200

    Added a fix to timeouts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 const { defaults } = require('jest-config')
 
 module.exports = {
+  setupFilesAfterEnv: ['jest.testSetup.js'],
   "testEnvironment": "node",
   "coveragePathIgnorePatterns": [
     ...defaults.coveragePathIgnorePatterns,
