586b5b1f3f7a285093b40c03371de5c7d955060f
wametsol <waltteri.metsola@gmail.com>
2019-03-22 16:52:42 +0200

    Adding participant to admin removes that user from participant list

--- controllers/emitter.js
+++ controllers/emitter.js
@@ -6,12 +6,6 @@ const { app } = require('../index')
 const { userEqualsUser } = require('../utils/userHandler')
 const http = require('http')
 const server = http.createServer(app)
-/*
-server.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`)
-  console.log(`API root starts at ${config.apiUrl}`)
-})
-*/
 
 server.on('start', () => {
   console.log('emitter started!')
